29a9dde6529054189e4df3569c0758d9ce8fdba9
Jason Music <JMUSIC@capellauniversity.edu>
2019-03-07 13:36:59 -0500

    Add project files.

---
+++ U09D1-ASP.Net-Core-Web-Application/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace U09D1_ASP.Net_Core_Web_Application.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ U09D1-ASP.Net-Core-Web-Application/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace U09D1_ASP.Net_Core_Web_Application.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
